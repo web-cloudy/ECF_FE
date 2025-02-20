acf4e2f36e45603786816b299b25c84f7846b56d
web-cloudy <nativedev124@gmail.com>
2025-02-20 15:48:25 +0900

    updated log parts in right panel

--- src/components/pricing/RightSidePanel.tsx
+++ src/components/pricing/RightSidePanel.tsx
@@ -136,85 +136,243 @@ export default function RightSidePanel() {
           <Typography variant="h5" mb={1}>
             Pricing
           </Typography>
-          <Box
-            sx={{ border: 1, borderColor: "divider", p: 2, borderRadius: 3 }}
-            className="flex flex-wrap gap-2"
+          <Grid2
+            container
+            spacing={2}
+            sx={{
+              border: 1,
+              borderColor: "divider",
+              padding: 2,
+              borderRadius: 3,
+            }}
           >
-            <Box
+            <Grid2
               sx={{ border: 1, borderColor: "divider", p: 1, borderRadius: 3 }}
-              className="min-w-1/3"
+              size={4}
             >
               <Typography variant="h6">Max Logn Amount</Typography>
               <Typography variant="h5">$40,000.00</Typography>
-            </Box>
-            <Box
+            </Grid2>
+            <Grid2
               sx={{ border: 1, borderColor: "divider", p: 1, borderRadius: 3 }}
-              className="min-w-1/3"
+              size={4}
             >
               <Typography variant="h6">Final Buy Rate</Typography>
               <Typography variant="h5">$100,000.00</Typography>
-            </Box>
-            <Box
+            </Grid2>
+            <Grid2
               sx={{ border: 1, borderColor: "divider", p: 1, borderRadius: 3 }}
+              size={4}
               className="min-w-1/3"
             >
               <Typography variant="h6">Price Adjustment</Typography>
               <Typography variant="h5">$150,000.00</Typography>
-            </Box>
-            <Box
+            </Grid2>
+            <Grid2
               sx={{ border: 1, borderColor: "divider", p: 1, borderRadius: 3 }}
-              className="min-w-1/2"
+              size={6}
             >
               <Typography variant="h6">Risk Score</Typography>
               <Typography variant="h5">$44,000.00</Typography>
-            </Box>
-            <Box
+            </Grid2>
+            <Grid2
               sx={{ border: 1, borderColor: "divider", p: 1, borderRadius: 3 }}
-              className="min-w-1/2"
+              size={6}
             >
               <Typography variant="h6">Risk Bucket</Typography>
               <Typography variant="h5">$80,000.00</Typography>
+            </Grid2>
+          </Grid2>
+          <Box
+            sx={{
+              border: 1,
+              borderColor: "divider",
+              padding: 2,
+              borderRadius: 3,
+              marginTop: 2,
+            }}
+          >
+            <Typography variant="h5">Levarages</Typography>
+            <TableContainer>
+              <Table>
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Indicator</TableCell>
+                    <TableCell>LTV (As is)</TableCell>
+                    <TableCell>Program 2</TableCell>
+                    <TableCell>Program 3</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  <TableRow>
+                    <TableCell>Max</TableCell>
+                    <TableCell>70%</TableCell>
+                    <TableCell>70%</TableCell>
+                    <TableCell>70%</TableCell>
+                  </TableRow>
+                  <TableRow>
+                    <TableCell>Adjusted Max</TableCell>
+                    <TableCell>70%</TableCell>
+                    <TableCell>40%</TableCell>
+                    <TableCell>60%</TableCell>
+                  </TableRow>
+                  <TableRow>
+                    <TableCell>Requested</TableCell>
+                    <TableCell>60%</TableCell>
+                    <TableCell></TableCell>
+                    <TableCell></TableCell>
+                  </TableRow>
+                </TableBody>
+              </Table>
+            </TableContainer>
+            <Grid2 container marginTop={2} spacing={2}>
+              <Grid2
+                size={6}
+                sx={{
+                  p: 2,
+                  border: 1,
+                  borderColor: "divider",
+                  borderRadius: 3,
+                }}
+              >
+                <Typography variant="h6">Profitability</Typography>
+                <Typography variant="h5">$450,000.00</Typography>
+              </Grid2>
+              <Grid2
+                size={6}
+                sx={{
+                  p: 2,
+                  border: 1,
+                  borderColor: "divider",
+                  borderRadius: 3,
+                }}
+              >
+                <Typography variant="h6">Rent Yield</Typography>
+                <Typography variant="h5">$450,000.00</Typography>
+              </Grid2>
+            </Grid2>
+          </Box>
+          <Box
+            sx={{
+              p: 2,
+              border: 1,
+              borderColor: "divider",
+              borderRadius: 3,
+              marginTop: 2,
+            }}
+            className="flex flex-col gap-3"
+          >
+            <Box
+              sx={{
+                p: 2,
+                border: 1,
+                borderColor: "divider",
+                borderRadius: 3,
+                backgroundColor: "#F6FAEA",
+              }}
+            >
+              <Typography variant="body1">Final Price</Typography>
+              <Typography variant="h5">104,512%</Typography>
+            </Box>
+            <Box
+              sx={{
+                p: 2,
+                border: 1,
+                borderColor: "divider",
+                borderRadius: 3,
+              }}
+              className="flex justify-between items-center"
+            >
+              <Typography variant="body1">Bise Price</Typography>
+              <Typography variant="body1">103.887</Typography>
+            </Box>
+            <Box
+              sx={{
+                p: 2,
+                border: 1,
+                borderColor: "divider",
+                borderRadius: 3,
+              }}
+              className="flex justify-between items-center"
+            >
+              <Typography variant="body1">Actual Interest Rato</Typography>
+              <Typography variant="body1">0.08</Typography>
+            </Box>
+            <Box
+              sx={{
+                p: 2,
+                border: 1,
+                borderColor: "divider",
+                borderRadius: 3,
+              }}
+              className="flex justify-between items-center"
+            >
+              <Typography variant="body1">
+                Prepayment Penalty - Max Price
+              </Typography>
+              <Typography variant="body1">105.1</Typography>
             </Box>
           </Box>
           <Box
-            sx={{ border: 1, borderColor: "divider", p: 2, borderRadius: 3 }}
-            className="flex flex-wrap gap-2"
+            sx={{
+              p: 2,
+              border: 1,
+              borderColor: "divider",
+              borderRadius: 3,
+              marginTop: 2,
+            }}
+            className="flex flex-col gap-3"
           >
-            <Typography variant="h6">Max Logn Amount</Typography>
+            <Typography variant="h5" mb={1}>
+              Levarages Adjustment
+            </Typography>
             <Box
-              sx={{ border: 1, borderColor: "divider", p: 1, borderRadius: 3 }}
-              className="min-w-1/3"
+              sx={{
+                p: 2,
+                border: 1,
+                borderColor: "divider",
+                borderRadius: 3,
+              }}
+              className="flex justify-between items-center"
             >
-              <Typography variant="h6">Max Logn Amount</Typography>
-              <Typography variant="h5">$40,000.00</Typography>
+              <Typography variant="body1">Total Adjustments</Typography>
+              <Typography variant="body1">$410,000.00</Typography>
             </Box>
             <Box
-              sx={{ border: 1, borderColor: "divider", p: 1, borderRadius: 3 }}
-              className="min-w-1/3"
+              sx={{
+                p: 2,
+                border: 1,
+                borderColor: "divider",
+                borderRadius: 3,
+              }}
+              className="flex justify-between items-center"
             >
-              <Typography variant="h6">Final Buy Rate</Typography>
-              <Typography variant="h5">$100,000.00</Typography>
+              <Typography variant="body1">Total Negative</Typography>
+              <Typography variant="body1">$80,000.00</Typography>
             </Box>
             <Box
-              sx={{ border: 1, borderColor: "divider", p: 1, borderRadius: 3 }}
-              className="min-w-1/3"
+              sx={{
+                p: 2,
+                border: 1,
+                borderColor: "divider",
+                borderRadius: 3,
+              }}
+              className="flex justify-between items-center"
             >
-              <Typography variant="h6">Price Adjustment</Typography>
-              <Typography variant="h5">$150,000.00</Typography>
+              <Typography variant="body1">Total Positive</Typography>
+              <Typography variant="body1">$210,000.00</Typography>
             </Box>
             <Box
-              sx={{ border: 1, borderColor: "divider", p: 1, borderRadius: 3 }}
-              className="min-w-1/2"
+              sx={{
+                p: 2,
+                border: 1,
+                borderColor: "divider",
+                borderRadius: 3,
+              }}
+              className="flex justify-between items-center"
             >
-              <Typography variant="h6">Risk Score</Typography>
-              <Typography variant="h5">$44,000.00</Typography>
-            </Box>
-            <Box
-              sx={{ border: 1, borderColor: "divider", p: 1, borderRadius: 3 }}
-              className="min-w-1/2"
-            >
-              <Typography variant="h6">Risk Bucket</Typography>
-              <Typography variant="h5">$80,000.00</Typography>
+              <Typography variant="body1">Median FICO</Typography>
+              <Typography variant="body1">$30,000.00</Typography>
             </Box>
           </Box>
         </CustomTabPanel>
